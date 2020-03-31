c8d41d20e9b4eaca9b994c5b5b677c94cfa61180
mikemey <miguel.maestro@gmx.co.uk>
2020-03-31 13:26:45 +0100

    refactor all fitness calculation in one class, add bonus on box-move

--- src/nn_player.py
+++ src/nn_player.py
@@ -5,37 +5,9 @@ from game_engine import BoxPusherEngine, Direction, MOVE_VECTOR, GameListener, p
 from training_levels import Level, distance_between
 
 
-class PenaltyCalculator(GameListener):
-    def __init__(self, verbose=False):
-        super().__init__()
-        self.verbose = verbose
-        self.unmoved_box_penalty = 10
-        self.penalty = self.unmoved_box_penalty
-        self.covered_positions = []
-
-    def __log__(self, *data):
-        if self.verbose:
-            print(*data)
-
-    def new_position(self, pos):
-        self.__log__(' new pos:', pos)
-        if positions_contains(self.covered_positions, pos):
-            self.__log__('path pen: 1')
-            self.penalty += 1
-        else:
-            self.covered_positions.append(pos.copy())
-
-    def box_move(self):
-        if self.unmoved_box_penalty:
-            self.__log__(' BOX move')
-            self.penalty -= self.unmoved_box_penalty
-            self.unmoved_box_penalty = False
-
-
 class NeuralNetMaster:
     def __init__(self):
         self.level = Level.generate_level()
-        self.level_box_error = distance_error(self.level.goal, self.level.boxes)
 
     def __create_game__(self, genome, config):
         engine = BoxPusherEngine(self.level.as_game_config())
@@ -45,13 +17,10 @@ class NeuralNetMaster:
 
     def eval_genome(self, genome, config):
         engine, player = self.__create_game__(genome, config)
-        calculator = PenaltyCalculator()
-        engine.listeners.add(calculator)
+        calculator = FitnessCalculator(engine, self.level)
         while not engine.game_over():
             player.next_move(engine)
-
-        box_error = 10 * (distance_error(engine.goal, engine.boxes) / self.level_box_error)
-        genome.fitness = engine.points - box_error - calculator.penalty
+        genome.fitness = calculator.get_fitness()
 
     def showcase_genome(self, genome, config):
         print('Showcase genome: {}, fitness: {}'.format(genome.key, genome.fitness))
@@ -98,11 +67,45 @@ class NeuralNetPlayer(AutoPlayer):
         engine.player_move(self.directions[direction_ix])
 
 
-def distance_error(goal, boxes):
-    err = 0
-    for box in boxes:
-        err += distance_between(goal, box)
-    return err
+class FitnessCalculator(GameListener):
+    def __init__(self, engine: BoxPusherEngine, level: Level, verbose=False):
+        super().__init__()
+        self.engine = engine
+        self.verbose = verbose
+        self.score = level.max_points
+        self.covered_positions = []
+
+        self.level_box_error = self.__current_box_error__()
+        self.engine.listeners.add(self)
+        self.__log__('start score:', self.score)
+
+    def __current_box_error__(self):
+        err = 0
+        for box in self.engine.boxes:
+            err += distance_between(box, self.engine.goal)
+        return err
+
+    def __log__(self, *data):
+        if self.verbose:
+            print(*data)
+
+    def new_position(self, pos):
+        self.__log__(' new pos:', pos)
+        self.score -= 1
+        if positions_contains(self.covered_positions, pos):
+            self.__log__('path penalty: 1')
+            self.score -= 1
+        else:
+            self.covered_positions.append(pos.copy())
+
+    def box_move(self):
+        self.__log__(' BOX move')
+        self.score += 5
+
+    def get_fitness(self):
+        box_error = 15 * self.__current_box_error__() / self.level_box_error
+        self.__log__('BOX penalty:', box_error)
+        return self.score - box_error
 
 
 if __name__ == '__main__':
