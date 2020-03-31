825f20be5e6ca2aeeef116cab0055740bb4caf42
mikemey <miguel.maestro@gmx.co.uk>
2020-03-31 21:15:01 +0100

    add rolling average to reporter

--- src/training.py
+++ src/training.py
@@ -1,7 +1,6 @@
 import math
 import os
 from datetime import datetime
-from statistics import mean
 
 import neat
 from neat.reporting import BaseReporter
@@ -38,32 +37,45 @@ class Trainer:
 
 class BPReporter(BaseReporter):
     def __init__(self):
-        self.gens_collected = 0
-        self.batch_fitness = []
-        self.batch_best = -math.inf
-        self.batch_best_size = None
+        self.generations = 0
+        self.total_fit = 0
+        self.total_pop = 0
+
+        self.batch_fit, self.batch_pop, self.batch_best, self.batch_best_size \
+            = self.__reset_batch__()
         self.__report__('--- START ---')
 
     def start_generation(self, generation):
-        self.gens_collected += 1
+        self.generations += 1
+
+    def __reset_batch__(self):
+        self.batch_fit = 0
+        self.batch_pop = 0
+        self.batch_best = -math.inf
+        self.batch_best_size = None
+        return self.batch_fit, self.batch_pop, self.batch_best, self.batch_best_size
 
     def post_evaluate(self, config, population, species_set, best_genome):
         gen_fitness = [genome.fitness for genome in population.values()]
-        self.batch_fitness.extend(gen_fitness)
+        self.batch_fit += sum(gen_fitness)
+        self.batch_pop += len(gen_fitness)
 
         if self.batch_best < best_genome.fitness:
             self.batch_best = best_genome.fitness
             self.batch_best_size = best_genome.size()
 
-        if (self.gens_collected % SUMMARIZE_GENS) == 0:
-            fit_mean = mean(self.batch_fitness)
-            self.__report__('{:5} gens {:4} / {:2}  --  avg/best  {:4.0f} / {:4.0f}  {}'.format(
-                self.gens_collected, len(population), len(species_set.species),
-                fit_mean, self.batch_best, self.batch_best_size)
-            )
+        if (self.generations % SUMMARIZE_GENS) == 0:
+            self.total_fit += self.batch_fit
+            self.total_pop += self.batch_pop
 
-            self.batch_fitness = []
-            self.batch_best = -math.inf
+            rolling_fit_mean = self.total_fit / self.total_pop
+            batch_fit_mean = self.batch_fit / self.batch_pop
+            self.__report__('{:5} gens, {:2} spc, avg r/b {:4.0f}/{:4.0f}, best: {:4.0f} {}'.format(
+                self.generations, len(species_set.species),
+                rolling_fit_mean, batch_fit_mean,
+                self.batch_best, self.batch_best_size)
+            )
+            self.__reset_batch__()
 
     @staticmethod
     def __report__(msg):
