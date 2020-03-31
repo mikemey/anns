adf383afec0637b38b7529a395a866af144f7e90
mikemey <miguel.maestro@gmx.co.uk>
2020-03-31 13:21:05 +0100

    move find-available-position function into level class

--- src/training_levels.py
+++ src/training_levels.py
@@ -19,12 +19,25 @@ class Level:
             field_size = (5, 5)
             # walls = [find_available_pos(occupied, 0, 5) for _ in range(random.randrange(0, 4))]
             walls = []
-            player = find_available_pos(occupied, 0, 5)
-            goal = find_available_pos(occupied, 0, 5, (2, 2), 1)
-            box = find_available_pos(occupied, 1, 4, goal, 3, True)
+            player = Level.find_available_pos(occupied, 0, 5)
+            goal = Level.find_available_pos(occupied, 0, 5, (2, 2), 1)
+            box = Level.find_available_pos(occupied, 1, 4, goal, 3, True)
             return Level(field_size, player, walls, [box], goal, max_points=20)
         except RecursionError:
             return Level.generate_level()
+
+    @staticmethod
+    def find_available_pos(occupied, start, exclusive_end, avoid_pos=None, min_dist=0, avoid_same_line=False):
+        new_pos = (random.randrange(start, exclusive_end, 1), random.randrange(start, exclusive_end, 1))
+        if new_pos in occupied:
+            return Level.find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
+        elif avoid_pos is not None and distance_between(new_pos, avoid_pos) < min_dist:
+            return Level.find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
+        elif avoid_same_line and (new_pos[0] == avoid_pos[0] or new_pos[1] == avoid_pos[1]):
+            return Level.find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
+        else:
+            occupied.append(new_pos)
+            return new_pos
 
     def as_game_config(self):
         return {
@@ -48,19 +61,6 @@ class Level:
         for line in reversed(field):
             print(intend, ''.join(line))
         print(separator)
-
-
-def find_available_pos(occupied, start, exclusive_end, avoid_pos=None, min_dist=0, avoid_same_line=False):
-    new_pos = (random.randrange(start, exclusive_end, 1), random.randrange(start, exclusive_end, 1))
-    if new_pos in occupied:
-        return find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
-    elif avoid_pos is not None and distance_between(new_pos, avoid_pos) < min_dist:
-        return find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
-    elif avoid_same_line and (new_pos[0] == avoid_pos[0] or new_pos[1] == avoid_pos[1]):
-        return find_available_pos(occupied, start, exclusive_end, avoid_pos, min_dist, avoid_same_line)
-    else:
-        occupied.append(new_pos)
-        return new_pos
 
 
 def distance_between(pos_a, pos_b):
