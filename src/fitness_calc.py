b489616c451d16091efd11e0e9fc0d2b078085ca
mikemey <miguel.maestro@gmx.co.uk>
2020-04-01 16:22:01 +0100

    add player-to-box distance penalty, update box moves rewards, increase covered-path penalty

--- src/fitness_calc.py
+++ src/fitness_calc.py
@@ -10,14 +10,14 @@ class FitnessCalculator(GameListener):
         self.score = level.max_points
         self.covered_positions = []
 
-        self.level_box_error = self.__current_box_error__()
+        self.level_box_error = self.__current_box_error_to__(self.engine.goal)
         self.engine.listeners.add(self)
         self.__log__('-- start --')
 
-    def __current_box_error__(self):
+    def __current_box_error_to__(self, pos):
         err = 0
         for box in self.engine.boxes:
-            err += distance_between(box, self.engine.goal)
+            err += distance_between(box, pos)
         return err
 
     def __log__(self, *data):
@@ -29,16 +29,16 @@ class FitnessCalculator(GameListener):
         self.__log__('new pos:', pos)
         for p in self.covered_positions:
             if (p == pos).all():
-                self.score -= 1
+                self.score -= 2
                 self.__log__('field covered')
         self.covered_positions.append(pos.copy())
 
     def box_move(self):
-        self.score += 5
+        self.score += 10
         self.__log__('box move')
 
     def box_in_goal(self):
-        self.score += 10
+        self.score += 30
         self.__log__('box in goal')
 
     def invalid_move(self):
@@ -46,6 +46,8 @@ class FitnessCalculator(GameListener):
         self.__log__('invalid move')
 
     def get_fitness(self):
-        box_error = 40 * (self.__current_box_error__() / self.level_box_error) ** 2
-        self.__log__('BOX penalty:', box_error)
-        return self.score - box_error
+        box_error = 50 * (self.__current_box_error_to__(self.engine.goal) / self.level_box_error) ** 2
+        player_error = 50 * (self.__current_box_error_to__(self.engine.player) - len(self.engine.boxes)) ** 2
+        self.__log__('box    error:', box_error)
+        self.__log__('player error:', player_error)
+        return self.score - box_error - player_error
