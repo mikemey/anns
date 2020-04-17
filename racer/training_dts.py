659d6328a542f0312c7a7ea559ea79c2e1df9b0a
mikemey <miguel.maestro@gmx.co.uk>
2020-04-17 10:33:13 +0100

    update training-delta-times generator (new sampling data), add print distribution from sample-file

--- racer/training_dts.py
+++ racer/training_dts.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-MEANS = [0.017, 0.025]
-DEVIATIONS = [0.001, 0.015]
-DISTRIBUTION_RATIOS = [0.75]
+MEANS = [0.017, 0.017, 0.032]
+DEVIATIONS = [0.0005, 0.002, 0.01]
+DISTRIBUTION_RATIOS = [0.6, 0.98]
 
 LIMITS_DIGITS = 3
 LIMIT_LOW = 0.010
-LIMIT_HIGH = 0.050
+LIMIT_HIGH = 0.060
 
 
 def random_dt():
@@ -36,75 +36,76 @@ def __pick_ix(ix=None):
 
 
 # Distribution sample printing:
-SAMPLE_SIZE = 600
-DELTA_TIME_STEP = 0.001
-
-DT_FORMAT = '{:.3f}'
-LOG_HEADER = 'time\t' + '\t'.join(['dist_{}'.format(i + 1) for i in range(len(MEANS))]) + '\ttotal'
-DT_LOG = DT_FORMAT + '\t{}' * (len(MEANS) + 1)
-EMPTY_DT_LOG = DT_FORMAT + '\t0' * (len(MEANS) + 1)
-
-START_COUNTS = [0] * len(MEANS)
+SAMPLE_SIZE = 5000
+INIT_COUNTERS = [0] * len(MEANS)
 
 
-def __print_dts_distribution():
+def __print_training_distribution():
     distribution = {}
     for _ in range(0, SAMPLE_SIZE):
         # ix = 0
         ix = __pick_ix()
         dt = np.round(__random_dt(ix), LIMITS_DIGITS)
-        times = distribution.setdefault(dt, START_COUNTS.copy())
+        times = distribution.setdefault(dt, INIT_COUNTERS.copy())
         times[ix] += 1
         distribution.update(({dt: times}))
 
-    print(LOG_HEADER)
+    __print_distribution(distribution)
+
+
+def __print_distribution_from(file):
+    with open(file) as f:
+        distribution = {}
+        for line in f.readlines():
+            dt = round(float(line), 3)
+            times = distribution.setdefault(dt, [0])
+            times[0] += 1
+            distribution.update(({dt: times}))
+        __print_distribution(distribution)
+
+
+DELTA_TIME_STEP = 0.001
+DT_FORMAT = '{:.3f}'
+
+
+# LOG_HEADER = 'time\t' + '\t'.join(['dist_{}'.format(i + 1) for i in range(len(MEANS))]) + '\ttotal'
+# DT_LOG = DT_FORMAT + '\t{}' * (len(MEANS) + 1)
+# EMPTY_DT_LOG = DT_FORMAT + '\t0' * (len(MEANS) + 1)
+
+
+def __create_table_templates(distribution):
+    distribution_width = len(list(distribution.values())[0])
+    header = 'time\t' + '\t'.join(['dist_{}'.format(i + 1) for i in range(distribution_width)]) + '\ttotal'
+    distribution_and_total_width = distribution_width + 1
+    log_line = DT_FORMAT + '\t{}' * distribution_and_total_width
+    empty_log = DT_FORMAT + '\t0' * distribution_and_total_width
+    return header, log_line, empty_log
+
+
+def __print_distribution(distribution):
+    header, dt_log_line, empty_dt_log = __create_table_templates(distribution)
+    print(header)
+    total_count = 0
     last_time = LIMIT_LOW
     sorted_dist = sorted(list(distribution.items()))
     for key, counters in sorted_dist:
         curr_time = float(key)
+        if curr_time > LIMIT_HIGH:
+            continue
         while curr_time > last_time:
-            print(EMPTY_DT_LOG.format(last_time))
+            print(empty_dt_log.format(last_time))
             last_time += DELTA_TIME_STEP
-        print(DT_LOG.format(key, *counters, sum(counters)))
+        time_counts = sum(counters)
+        total_count += time_counts
+        print(dt_log_line.format(key, *counters, time_counts))
         last_time += DELTA_TIME_STEP
 
     while last_time <= LIMIT_HIGH + DELTA_TIME_STEP:
-        print(EMPTY_DT_LOG.format(last_time))
+        print(empty_dt_log.format(last_time))
         last_time += DELTA_TIME_STEP
+    print('Total count:', total_count)
 
 
 if __name__ == '__main__':
-    __print_dts_distribution()
-
-# Recorded delta-times to replicate (single-threaded, obsolete after parallel processing):
-# 0.009	1
-# 0.010	1
-# 0.011	0
-# 0.012	0
-# 0.013	1
-# 0.014	3
-# 0.015	5
-# 0.016	67
-# 0.017	84
-# 0.018	16
-# 0.019	6
-# 0.020	2
-# 0.021	2
-# 0.023	2
-# 0.024	3
-# 0.025	1
-# 0.026	1
-# 0.027	1
-# 0.028	1
-# 0.029	1
-# 0.030	0
-# 0.031	4
-# 0.032	1
-# 0.033	3
-# 0.034	3
-# 0.035	2
-# 0.036	4
-# 0.037	7
-# 0.038	2
-# 0.039	4
-# 0.04	1
+    __print_training_distribution()
+    # __print_distribution_from('real-dts.txt')
