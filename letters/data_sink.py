350ad89c6ed930eb68a5a49bbce8be4185ad7bc8
mikemey <miguel.maestro@gmx.co.uk>
2020-06-23 15:52:11 +0100

    implement data-sink log-dir creation + graph-header writing

--- letters/data_sink.py
+++ letters/data_sink.py
@@ -1,3 +1,30 @@
-class DataSink():
-    def __init__(self):
-        self.name = 'hello world'
+import pathlib
+from functools import reduce
+
+LOG_DIR = 'log'
+
+FNAME = 'file_name'
+
+
+def comma_separated(total, current):
+    return f'{total},{current}'
+
+
+class DataSink:
+    def __init__(self, run_id, log_dir=LOG_DIR):
+        self.log_dir = f'{log_dir}/{run_id}'
+        self.graphs = {}
+        log_path = pathlib.Path(self.log_dir)
+        if log_path.exists():
+            raise AssertionError(f'duplicate run-id, log-dir: {self.log_dir}')
+        log_path.mkdir(parents=True, exist_ok=True)
+
+    def add_graph_header(self, graph_name, *fields):
+        if graph_name in self.graphs.keys():
+            raise AssertionError(f'duplicate graph name: {graph_name}')
+
+        fname = f'{self.log_dir}/{graph_name}.csv'
+        self.graphs[graph_name] = {FNAME: fname}
+        header = reduce(comma_separated, fields)
+        with open(fname, 'a+') as graph_f:
+            graph_f.write(f'{header}\n')
--- letters/tests/data_sink_test.py
+++ letters/tests/data_sink_test.py
@@ -1,13 +1,52 @@
+import os
+import shutil
 import unittest
 
 from data_sink import DataSink
 
+test_log_dir = f'{os.path.dirname(__file__)}/log'
+field_x, field_y1, field_y2 = 'field_x', 'field_y_1', 'field_y_2'
 
-class LogTestCase(unittest.TestCase):
-    def setUp(self):
-        print('hello')
 
-    def test_blabla(self):
-        sink = DataSink()
-        self.assertEqual(sink.name, 'hello world')
-        print('\n-- test DONE\n')
+class TestDataSink(DataSink):
+    def __init__(self, run_id):
+        super().__init__(run_id, test_log_dir)
+
+
+class DataSinkTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        shutil.rmtree(test_log_dir, ignore_errors=True)
+
+    def test_creates_log_files(self):
+        run_test_id, graph_id_1, graph_id_2 = 'run-1', 'graph1', 'graph2'
+        sink = TestDataSink(run_test_id)
+        sink.add_graph_header(graph_id_1, field_x, field_y2)
+        sink.add_graph_header(graph_id_2, field_x, field_y1, field_y2)
+
+        def assert_header_line(graph_id, expected_header):
+            with open(f'{test_log_dir}/{run_test_id}/{graph_id}.csv') as f:
+                lines = f.readlines()
+                self.assertEqual(1, len(lines))
+                self.assertEqual(expected_header, lines[0])
+
+        assert_header_line(graph_id_1, f'{field_x},{field_y2}\n')
+        assert_header_line(graph_id_2, f'{field_x},{field_y1},{field_y2}\n')
+
+    def test_reject_duplicate_run_id(self):
+        dup_run_id = 'duplicate-name'
+        TestDataSink(dup_run_id)
+        with self.assertRaises(AssertionError) as ctx:
+            TestDataSink(dup_run_id)
+        self.assertIn(f'{dup_run_id}', str(ctx.exception))
+
+    def test_reject_duplicate_graph_id(self):
+        sink = TestDataSink('run-2')
+        dup_graph_id = 'dup-graph'
+        sink.add_graph_header(dup_graph_id, 'x')
+        with self.assertRaises(AssertionError) as ctx:
+            sink.add_graph_header(dup_graph_id, 'x')
+        self.assertIn(f'{dup_graph_id}', str(ctx.exception))
+
+    # def test_writes_to_file_in_batches(self):
+    #     pass
